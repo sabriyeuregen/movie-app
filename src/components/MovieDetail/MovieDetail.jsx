67515459c5c65ea7fb4e471591eba2f6364b5a77
sabriyeuregen <sabriyeuregen@gmail.com>
2023-04-21 22:40:24 +0300

    Updated : Movie Detail

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -2,46 +2,47 @@ import { useState, useEffect } from "react";
 import MovieCart from "../MovieCart/MovieCart";
 import { useParams } from "react-router-dom";
 const MovieDetail = () => {
-
-  const [ detail, setDetail] = useState("");
+  const [detail, setDetail] = useState([]);
 
   const { id } = useParams();
 
-  const getMovie = async () => {
+  const getMovie = async (id) => {
     const movie = await fetch(
       `https://api.themoviedb.org/3/movie/${id}?api_key=8007da3ba2c47e0304c3a0e70c97a6b8`
-      )
-      .then((response) => response.json())
-      .then((response) => response.results);
+    )
+      .then((response) => response)
+      .then((response) => response.json());
       setDetail(movie);
-      console.log(movie+"movie")
   };
+  console.log(detail)
 
   useEffect(() => {
     getMovie(id ? parseInt(id,10) : 0);
+    
   }, []);
 
-  console.log(id)
-  console.log(detail+ "data")
+  const IMAGES_PATH = "https://image.tmdb.org/t/p";
 
- /* const movieDetail = detail.map((movie) => (
+ const movieDetail = detail.map((movie) => (
     <MovieCart
       key={movie.id}
       link={
-        <Link to={`/movie/${movie.id}`}>
-          {movie.poster_path && (
-            <img
-              src={`${IMAGES_PATH}/w300${movie.poster_path}`}
-              alt={movie.title}
-            />
-          )}
-        </Link>
+        movie.poster_path && (
+          <img
+            src={`${IMAGES_PATH}/w300${movie.poster_path}`}
+            alt={movie.title}
+          />
+        )
       }
       title={movie.title}
     />
-  ));*/
+  ));
 
-  return <div>{/*movieDetail*/}</div>;
+  return (
+    <div>
+      {detail && movieDetail}
+    </div>
+  );
 };
 
 export default MovieDetail;
